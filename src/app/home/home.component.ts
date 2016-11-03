23e34d088f72e58212930e4f7637cf5951729845
Cristian Buffa <bufface@gmail.com>
2016-11-03 13:33:30 -0300

    Mostrar datos en un componente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  logros: ILogro[];
+  title: string = 'Bienvenido';
+
   constructor() { }
 
   ngOnInit() {
+    this.logros = this.getLogros();
   }
 
+  getLogros(): ILogro[] {
+    return [{
+      id: 1,
+      title: 'Primer Logro',
+      description: 'Description Primer Logro'
+    },{
+      id: 2,
+      title: 'Segundo Logro',
+      description: 'Description Segundo Logro'
+    },{
+      id: 2,
+      title: 'Tercer Logro',
+      description: 'Description Tercer Logro'
+    }];
+  }
+}
+
+interface ILogro {
+  id: number;
+  title: string;
+  description?: string;
 }
